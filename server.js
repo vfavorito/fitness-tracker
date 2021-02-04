b105845397db2851e63e376d7f77c8a766e06b10
vfavorito <vince.favorito@gmail.com>
2021-02-03 19:48:10 -0600

    upgraded file structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const APIrouter = require("./routes/api-routes");
+const HTMLrouter = require("./routes/html-routes");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(APIrouter);
+app.use(HTMLrouter);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
