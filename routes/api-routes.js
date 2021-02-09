c7d29dd5a9b369695bff73699f9b7898669c9078
vfavorito <vince.favorito@gmail.com>
2021-02-08 20:36:44 -0600

    added comments to the api-routes.js file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,7 @@ const APIrouter = express.Router();
 const mongoose = require("mongoose");
 const Workout = require("../models/Workout")
 
+// grabbing all workout documents and sending them to the front to manipulate
 APIrouter.get("/api/workouts", (req, res) => {
     Workout.find({})
         .then((workouts) => {
@@ -12,7 +13,7 @@ APIrouter.get("/api/workouts", (req, res) => {
             res.json(error);
         })
 });
-
+// grabbing all workout documents and sending them to the front to manipulate
 APIrouter.get("/api/workouts/range", (req, res) => {
     Workout.find({})
         .then((workouts) => {
@@ -22,7 +23,10 @@ APIrouter.get("/api/workouts/range", (req, res) => {
             res.json(error);
         });
 });
-
+// receiving exercise data from the front via the body of a request 
+// then finding the workout document that has the same id that is in our url (req.params)
+// then updating that document with the data we recieved from the body of the request 
+// then returning the updated document
 APIrouter.put("/api/workouts/:id", (req, res) => {
     exerciseData = req.body;
     Workout.findOneAndUpdate({ _id: mongoose.Types.ObjectId(req.params.id) }, { $push: { exercises: exerciseData } }, { new: true })
@@ -34,7 +38,8 @@ APIrouter.put("/api/workouts/:id", (req, res) => {
             res.json(error);
         });
 });
-
+// creating a new blank "Workout" document in the database
+// then returning that new document
 APIrouter.post("/api/workouts", (req, res) => {
     const NewWorkout = new Workout();
     Workout.create(NewWorkout)
