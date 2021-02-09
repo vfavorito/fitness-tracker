88e27a19f9e1313859e15273ddaa3460c6cf6855
vfavorito <vince.favorito@gmail.com>
2021-02-08 20:38:49 -0600

    added comments to the html-routes.js file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const HTMLrouter = express.Router();
 const path = require("path");
-
+// when the "/" route is hit send the index.html file to display to the user
 HTMLrouter.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/html/index.html"));
 });
-
+// when the  "/exercise" route is hit send the exercise.html file to display to the user
 HTMLrouter.get("/exercise", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/html/exercise.html"))
 });
-
+// when the "/stats" route is hit send the stats.html file to display to the user
 HTMLrouter.get("/stats", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/html/stats.html"))
 })
